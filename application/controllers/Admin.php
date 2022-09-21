7b73cc88364b5dd318b7072189797ec4744c79c9
webprograming <alldii1956@gmail.com>
2022-09-21 16:40:39 +0700

    last

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -73,6 +73,72 @@
 			$this->session->set_flashdata('message', 'swal("Yess!", "Produk berhasil dihapus", "success" );');
 			redirect('admin/data_produk');
 		}
+
+		function data_kategori(){
+
+			$data['kategori'] = $this->db->get('tbl_kategori')->result_array();
+
+			$this->load->view('template_admin/header');
+			$this->load->view('admin/data_kategori', $data);
+			$this->load->view('template_admin/footer');
+		}
+
+		function tambah_kategori(){
+
+			$kode = "KT-".rand(0,1000);
+			$nama_kategori = $this->input->post('nama_kategori');     
+			$slug = str_replace(" ", "-", $nama_kategori);
+			$slug = ucwords($slug);
+
+			$data = [
+				'kode_kategori' => $kode,
+				'nama_kategori' => $nama_kategori,
+				'slug' => strtolower($slug),
+			];
+
+			$this->db->insert('tbl_kategori', $data);
+			$this->session->set_flashdata('message', 'swal("Yess!", "Kategori berhasil ditambah", "success" );');
+			redirect('admin/data_kategori');
+		}
+
+		function hapus_kategori(){
+
+			$id = $this->input->post('id');
+			$this->db->where('id', $id);
+			$this->db->delete('tbl_kategori');
+			$this->session->set_flashdata('message', 'swal("Yess!", "Kategori berhasil dihapus", "success" );');
+			redirect('admin/data_kategori');
+		}
+
+		function edit_kategori(){
+
+			$data = [
+				'nama_kategori' => $this->input->post('nama_kategori'),
+			];
+			$this->db->where('id', $this->input->post('id'));
+			$this->db->update('tbl_kategori', $data);
+			$this->session->set_flashdata('message', 'swal("Yess!", "Kategori berhasil diedit", "success" );');
+			redirect('admin/data_kategori');
+		}
+
+
+		function data_user(){
+
+			$data['user'] = $this->db->get('tbl_register')->result_array();
+
+			$this->load->view('template_admin/header');
+			$this->load->view('admin/data_user', $data);
+			$this->load->view('template_admin/footer');
+		}
+
+		function hapus_user(){
+
+			$id = $this->input->post('id');
+			$this->db->where('id', $id);
+			$this->db->delete('tbl_register');
+			$this->session->set_flashdata('message', 'swal("Yess!", "User berhasil dihapus", "success" );');
+			redirect('admin/data_user');
+		}
 	}
 
 ?>
